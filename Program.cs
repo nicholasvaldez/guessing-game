d39509c597c19f696396f7919c3028320cd02a74
Nick Valdez <nicholastvaldez@gmail.com>
2023-08-28 10:22:35 -0500

    add difficulty levels that changes the amount of guesses

--- Program.cs
+++ Program.cs
@@ -1,16 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 Console.WriteLine("Hello! Welcome to The Guessing Game!");
 Console.WriteLine("-----------------------");
 
 Random r = new Random();
-    int secretNumber = r.Next(0,100);
+int secretNumber = r.Next(0,100);
     // Console.WriteLine(secretNumber);
 
-for (int i = 0; i < 4; i++)
+List<int> difficultyLevels = new List<int>()
 {
-    int remainingGuesses = 3 - i;
+8, 6, 4
+};
+
+Console.WriteLine("Easy, Medium, or Hard? ");
+Console.Write("> ");
+
+string difficultyInput = Console.ReadLine().ToLower();
+
+int difficultyLevel = 0;
+
+if (difficultyInput == "easy")
+{
+     difficultyLevel = difficultyLevels[0];
+}
+else if (difficultyInput == "medium")
+{
+     difficultyLevel = difficultyLevels[1];
+
+}
+else if (difficultyInput == "hard")
+{
+     difficultyLevel = difficultyLevels[2];
+
+}
+
+
+
+for (int i = 0; i < difficultyLevel; i++)
+{
+    int remainingGuesses = difficultyLevel - i - 1;
     Console.Write("Please guess the secret number: ");
     
 
