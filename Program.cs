c7969bb0caf379c102ad9c8315fb528c651b8c4f
Nick Valdez <nicholastvaldez@gmail.com>
2023-08-28 09:13:56 -0500

    Add greeting and question requesting users guess as input

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 
-namespace GuessingGame
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Hello! Welcome to The Guessing Game!");
+Console.WriteLine("-----------------------");
+Console.Write("Please guess the secret number: ");
+string input = Console.ReadLine();
+Console.WriteLine($"Just to be sure... {input} is your guess?");
