854f667427c11359785ba85849926eaabdac0549
Nick Valdez <nicholastvaldez@gmail.com>
2023-08-28 12:49:22 -0500

    add cheater difficulty

--- Program.cs
+++ Program.cs
@@ -2,74 +2,83 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 
-Console.WriteLine("Hello! Welcome to The Guessing Game!");
-Console.WriteLine("-----------------------");
+Main();
 
-Random r = new Random();
-int secretNumber = r.Next(0,100);
-    // Console.WriteLine(secretNumber);
-
-List<int> difficultyLevels = new List<int>()
+void Main()
 {
-8, 6, 4
-};
+    Console.WriteLine("Hello! Welcome to The Guessing Game!");
+    Console.WriteLine("-----------------------");
 
-Console.WriteLine("Easy, Medium, or Hard? ");
-Console.Write("> ");
+    Random r = new Random();
+    int secretNumber = r.Next(0,100);
+        // Console.WriteLine(secretNumber);
 
-string difficultyInput = Console.ReadLine().ToLower();
-
-int difficultyLevel = 0;
-
-if (difficultyInput == "easy")
-{
-     difficultyLevel = difficultyLevels[0];
-}
-else if (difficultyInput == "medium")
-{
-     difficultyLevel = difficultyLevels[1];
-
-}
-else if (difficultyInput == "hard")
-{
-     difficultyLevel = difficultyLevels[2];
-
-}
-
-
-
-for (int i = 0; i < difficultyLevel; i++)
-{
-    int remainingGuesses = difficultyLevel - i - 1;
-    Console.Write("Please guess the secret number: ");
-    
-
-    string input = Console.ReadLine();
-    int parsedInput = int.Parse(input);
-
-
-    
-    
-    Console.WriteLine($"Guess #{i + 1}. {remainingGuesses} guesses remaining.");
-
-
-    if (parsedInput != secretNumber)
+    List<int> difficultyLevels = new List<int>()
     {
-        Console.WriteLine("Sorry, thats incorrect!");
-        if (parsedInput < secretNumber)
+    8, 6, 4
+    };
+
+    Console.WriteLine("Easy, Medium, or Hard? .... or Cheater? ");
+    Console.Write("> ");
+
+    string difficultyInput = Console.ReadLine().ToLower();
+
+    int difficultyLevel = 0;
+
+    if (difficultyInput == "easy")
+    {
+        difficultyLevel = difficultyLevels[0];
+    }
+    else if (difficultyInput == "medium")
+    {
+        difficultyLevel = difficultyLevels[1];
+
+    }
+    else if (difficultyInput == "hard")
+    {
+        difficultyLevel = difficultyLevels[2];
+
+    }
+    else if (difficultyInput == "cheater")
+    {
+        difficultyLevel = 100;
+
+    }
+
+
+    for (int i = 0; i < difficultyLevel; i++)
+    {
+        int remainingGuesses = difficultyLevel - i - 1;
+        Console.Write("Please guess the secret number between 0 to 100: ");
+        
+
+        string input = Console.ReadLine();
+        int parsedInput = int.Parse(input);
+
+
+        
+        
+        Console.WriteLine($"Guess #{i + 1}. {remainingGuesses} guesses remaining.");
+
+
+        if (parsedInput != secretNumber)
         {
-            Console.WriteLine("Too low!!");
+            Console.WriteLine("Sorry, thats incorrect!");
+            if (parsedInput < secretNumber)
+            {
+                Console.WriteLine("Too low!!");
+            }
+            else 
+            {
+                Console.WriteLine("Too High!!");
+            }
+            
         }
         else 
         {
-            Console.WriteLine("Too High!!");
+            Console.WriteLine("Buy a lottery ticket right now, that's correct!");
+            break;
         }
-        
-    }
-    else 
-    {
-        Console.WriteLine("Buy a lottery ticket right now, that's correct!");
-        break;
-    }
 
+    }
 }
